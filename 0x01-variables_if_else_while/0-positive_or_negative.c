793ab4e217f7fcdac4bdc593797cbadfa830376e
ZcMaragia <zmaragia@gmail.com>
2022-07-01 13:59:22 -0700

    If else for while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,7 @@
 /*
  *
  * File: 0-positive_or_negative.c
- *
- * Auth: Brennan D Baraban
+ * Auth: Zach Maragia
  *
  */
 
@@ -17,18 +16,18 @@
  *
  * Return: Always 0.
 */
-int main (void)
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if(n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
+	else if(n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	return (0);
+	return(0);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: Zach Maragia
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
