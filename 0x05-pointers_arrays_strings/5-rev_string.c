c81df75302ea5727b5bbe98c6f64cdf01f24b3b4
ZcMaragia <zmaragia@gmail.com>
2022-07-13 14:36:17 -0700

    arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+# include "main.h"
+/**
+ * rev_string - function reverses a string
+ *
+ * @s: characters
+ *
+ */
+void rev_string(char *s)
+
+{
+
+	int i = 0, len = 0;
+
+	char tmp;
+
+	while (s[i++])
+
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+
+	{
+
+		tmp = s[i];
+
+		s[i] = s[len - i - 1];
+
+		s[len - i - 1] = tmp;
+
+	}
+
+}
