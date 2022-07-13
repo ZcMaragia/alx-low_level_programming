ec772550445c34dd36fbca255f80df86590917c2
ZcMaragia <zmaragia@gmail.com>
2022-07-13 13:41:43 -0700

    three

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,16 @@
  * _strlen -give the length of a string
  *
  * @s: the string
+ *
  * Return: the length of a string.
  */
 
 int _strlen(char *s)
 {
 	int i = 0;
+	
 	while (s[i])
 	i++;
+	
 	return (i);
 }
